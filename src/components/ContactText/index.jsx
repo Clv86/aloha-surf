628b8d65c872c1fb2b2d795e1a17a21e01110d4f
Clv86 <charles-louis.velieu@tutanota.com>
2024-03-08 09:52:37 +0100

    Contact updated

--- src/components/ContactText/index.jsx
+++ src/components/ContactText/index.jsx
@@ -1,31 +1,21 @@
 import styled from 'styled-components'
 
 function ContactText() {
-  function sendMail() {
-    window.location.href = 'mailto:charleslouis.velieu@gmail.com'
-  }
   return (
     <ContactStyle>
       <ContactDescription>
         <ContactTitle>À propos</ContactTitle>
         J'ai fait ce site pour m'exercer au code et montrer les conditions des
         différents spots susceptibles d'être intéressant sur la côte ouest
-        française ! N'hésitez pas à me contacter pour faire des retours sur le
-        site ou proposer de nouveaux spots à ajouter :{' '}
-        <ContactMail onClick={sendMail}>
-          charleslouis.velieu@gmail.com
-        </ContactMail>
+        française !{' '}
+        <a href="mailto:charleslouis.velieu@gmail.com">
+          N'hésitez pas à me contacter pour faire des retours sur le site ou
+          proposer de nouveaux spots à ajouter !{' '}
+        </a>
       </ContactDescription>
     </ContactStyle>
   )
 }
-const ContactMail = styled.p`
-  cursor: pointer;
-
-  touch-action: manipulation;
-
-  // height: 10%;
-`
 const ContactStyle = styled.div`
   width: 444px;
   flex-direction: column;
@@ -54,17 +44,15 @@ const ContactTitle = styled.h1`
   margin: 0;
   @media (max-width: 1000px) {
     text-align: center;
-    // margin-left: 50%;
-    // margin-right: 50%;
   }
 `
 const ContactDescription = styled.p`
+  display: block;
   font-family: Amaranth;
   font-size: 16px;
   font-style: normal;
   font-weight: 400;
   line-height: normal;
-  // width: clamp(45ch, 50%, 75ch);
   height: 71px;
   margin: 0;
   margin-bottom: 10%;
--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -4,44 +4,49 @@ function Footer() {
   return (
     <FooterContact>
       <ContactTitle>Contact</ContactTitle>
-      <ContactText>loremipsum@gmx.fr</ContactText>
+      <ContactText href="mailto:charleslouis.velieu@gmail.com">
+        charleslouis.velieu@gmail.com
+      </ContactText>
     </FooterContact>
   )
 }
 
 const FooterContact = styled.div`
-display= inline;
-position: absolute;
-bottom: 0%;
-left: 0%;
-text-align: center;
-width: 100%;
-height: 8%;
-box-sizing: border-box;
-padding: 0px;
-z-index: 2;
-@media (max-height: 464px) {
-  display: none;
-}`
+  display: flex;
+  padding-bottom: 2%;
+  flex-direction: column;
+  justify-content: start;
+  align-items: center;
+
+  position: absolute;
+  bottom: 0%;
+  left: 0%;
+  text-align: center;
+  width: 100%;
+  box-sizing: border-box;
+  padding: 0px;
+  z-index: 2;
+  @media (max-height: 464px) {
+    display: none;
+  }
+`
 
 const ContactTitle = styled.h1`
-  padding: 0px;
-  margin: 0px;
   font-size: 24px;
   font-style: normal;
   font-weight: 400;
   line-height: normal;
+  margin: 0;
 `
-const ContactText = styled.p`
-  padding: 0px;
-  margin: 0px;
+const ContactText = styled.a`
+  text-decoration: none;
   color: #000;
-  text-align: center;
   font-family: Amaranth;
   font-size: 8px;
   font-style: normal;
   font-weight: 400;
   line-height: normal;
+  margin-bottom: 1%;
 `
 
 export default Footer

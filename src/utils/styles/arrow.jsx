854cc52c9bd4420660c724e44d4b981bc51d94dc
Clv86 <charles-louis.velieu@tutanota.com>
2024-05-24 12:50:51 +0200

    Arrow direction fixed

--- src/utils/styles/arrow.jsx
+++ src/utils/styles/arrow.jsx
@@ -2,14 +2,16 @@ import React from 'react'
 
 function Arrow(props) {
   return (
-    <svg
-      data-name="1-Arrow Up"
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 32 32"
-      style={{ width: '20px', ...props.style }}
-    >
-      <path d="m26.71 10.29-10-10a1 1 0 0 0-1.41 0l-10 10 1.41 1.41L15 3.41V32h2V3.41l8.29 8.29z" />
-    </svg>
+    <>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        style={{ width: '20px', ...props.style }}
+        width="24"
+        height="24"
+      >
+        <path d="M13 17.586V4h-2v13.586l-6.293-6.293-1.414 1.414L12 21.414l8.707-8.707-1.414-1.414L13 17.586z" />
+      </svg>
+    </>
   )
 }
 
